a81f70d66ca07bf9b079e11b56db1e9ad21517fc
Blair Reeves <gaslight@gmail.com>
2015-11-12 21:23:47 -0500

    initial

--- client/hills.js
+++ client/hills.js
@@ -25,6 +25,27 @@ chooseclear = function(num){
 	document.getElementById(num).innerHTML = "";
 }
 
+statusupdate = function(x){
+	if (thread <0.4){return}
+	if (userInfo.turnsToday < 0){userInfo.turnsToday = 0}
+	if (x==="dead"){
+		document.getElementById('status').innerHTML = "Player: 💀";
+		document.getElementById('status2').innerHTML = "HP: 💀" + " <><> " + "Gold: 0";
+	} else if (x==="reset"){
+		document.getElementById('status').innerHTML = "Player:";
+		document.getElementById('status2').innerHTML = "";
+	} else {
+		Meteor.call('playerUpdate', userInfo.username, userInfo, function(error,result){});
+		document.getElementById('status').innerHTML = "Player: " + userInfo.username;
+		if (flag==="fester"){
+			fester();
+			document.getElementById('status2').innerHTML = "HP: " + userInfo.hp + " <span id=defeat>Festering wounds!</span> <><> " + "Gold: " + userInfo.gold;
+		} else {
+		document.getElementById('status2').innerHTML = "HP: " + userInfo.hp + " <><> " + "Gold: " + userInfo.gold;
+		}
+	}
+}
+
 // ROUTER FUNCTION
 
 Template.hills.onRendered(function(){
@@ -34,10 +55,9 @@ $('form').submit(function(e){
 		e.preventDefault();
 		var userinput = $('input').val().toLowerCase();
 		$('input[type=text]').val("");
-		console.log(userinput);
-		console.log(thread);
-		// console.log(weapons.(UserInfo.item.weapon.name).attack);
-		// skip function
+		console.log("input: " + userinput);
+		console.log("thread: " + thread);
+		statusupdate();
 		if (userinput === "skip") { townsquare() }
 		else if (userinput==="return"){
 			console.log("return to start");
@@ -51,11 +71,19 @@ $('form').submit(function(e){
 				enterpath();
 				break;
 			case 0.2:
-				userInfo.username = userinput;
-				greeting(userinput);
+				login(userinput);
+				break;
+			case 0.21:
+				login1(userinput);
+				break;
+			case 0.22:
+				newaccount(userinput,1);
+				break;
+			case 0.23:
+				newaccount(userinput, 2);
 				break;
 			case 0.3:
-				greeting2(userinput);
+				greeting(userinput);
 				break;
 			case 0.4:
 				departure(userinput);
@@ -70,12 +98,18 @@ $('form').submit(function(e){
 				tavernrouter(userinput);
 				break;				
 			case 0.71:
+				tavern();
+				break;
+			case 0.711:
 				if (userinput === "q") tavern();
-				else barlines();
+				else drinkrouter(userinput);
 				break;
 			case 0.72:
 				if (userinput === "q") tavern();
 				else barlines();
+				break;
+			case 0.75:
+				minstrel(userinput);
 				break;
 			case 0.8:
 				smithrouter(userinput);
@@ -97,29 +131,37 @@ $('form').submit(function(e){
 			case 0.83:
 				smithy("a");
 				break;
+			case 0.9:
+				alchemyrouter(userinput);
+				break;
+			case 0.905:
+				alchemist("a");
+				break;
+			case 0.91:
+				alchconfirm(userinput, "heals");
+				break;
+			case 0.911:
+				alchpurch(userinput, "heals");
+				break;
+			case 0.912:
+				alchpurch(userinput, "meds");
+				break;
+			case 0.92:
+				alchconfirm(userinput, "meds");
+				break;
 			case 1:
-				if (userinput === 't') townsquare();
-				else if (userinput === "heal") {
-					userInfo.hp = 25;
-					woodsstart();
-					}
-				else if (userinput === "s") woodsstatus();
-				else woodsencounter();
+				woodsrouter(userinput);
 				break;
 			case 1.1:
 				if (userinput === 'r') woodsrun();
-				else woodsfight("a");
+				else woodsfight();
 				break;
 			case 1.2:
 				reward();
 				break;
-			case 1.21:
-				if (userinput === 'r') woodsrun();
-				else woodsfight("x");
-				break;
 			case 1.22:
 				if (userinput === 'r') woodsrun();
-				else woodsfight("a");
+				else woodsfight(userinput);
 				break;
 			case 1.23:
 				woodsstart();
@@ -127,42 +169,173 @@ $('form').submit(function(e){
 			case 1.24:
 				woodsfight("m");
 				break;
+			case 1.25:
+				usegear(userinput);
+				break;
+			case 1.251:
+				usegear(userinput, 1);
+				break;
 			case 1.9:
 				townsquare();
 				break;				
+			case 2:
+				baileyrouter(userinput);
+				break;
+			case 2.1:
+				bstrength(userinput);
+				break;
+			case 2.11:
+				bstrength1(userinput);
+				break;
+			case 2.12:
+				bstrength2(userinput);
+				break;
+			case 2.2:
+				bspeed(userinput);
+				break;
+			case 2.21:
+				bspeed1(userinput);
+				break;
+			case 2.22:
+				bspeed2(userinput);
+				break;
+			case 2.3:
+				royale("a");
+				break;
+			case 3:
+				rrouter(userinput);
+				break;
+			case 3.5:
+				bailey();
+				break;
+			case 4:
+				bankrouter(userinput);
+				break;
+			case 4.1:
+				bdeposit(userinput);
+				break;
+			case 4.2:
+				bank();
+				break;
+			case 4.3:
+				bwithdraw(userinput);
+				break;
+			case 5:
+				abbeyrouter(userinput);
+				break;
+			case 5.01:
+				townsquare();
+				break;
+			case 5.1:
+				abbey1(userinput);
+				break;		
+			case 5.2:
+				if (userinput === 'r') awoodsrun();
+				else awoodsfight("a");
+				break;
+			case 5.24:
+				awoodsfight("m");
+				break;				
+			case 5.3:
+				areward();
+				break;
+			case 5.4:
+				abbey();
+				break;
+			case 5.5:
+				afaith(1);
+				break;
+			case 5.51:
+				afaith(2);
+				break;
+			case 5.6:
+				abbeyup();
+				break;
+			case 5.61:
+				abbeyup(2,userinput);
+				break;
+			case 6:
+				farmrouter(userinput);
+				break;
+			case 6.1:
+				frouter1(userinput);
+				break;
+			case 6.12:
+				frouter1(userinput, 2);
+				break;
+			case 6.13:
+				frouter1(userinput, 3);
+				break;
+			case 6.14:
+				frouter1(userinput, 4);
+				break;
+			case 6.2:
+				frouter2(userinput);
+				break;
+			case 6.3:
+				farm2(userinput);
+				break;
+			case 7:
+				magerouter(userinput);
+				break;
+			case 7.1:
+				mageconfirm(userinput);
+				break;
+			case 7.2:
+				mpurch(userinput);
+				break;
+			case 7.7:
+				mage();
+				break;
 			case 86:
 				death();
 				break;			
+			case 86.1:
+				fester(1);
+				break;
 			case 99:
-				giles();
+				finale();
 				break;
 			default:
 				output("I don't understand.");
 				if (thread >= 0.6){ 
 					setTimeout(function(){ townsquare() }, 1000);
-		}	}
-	}
-});
-
+			}	}
+		}
+	});
 });
 
 // user variables
 
 thread = 0;
+userInfo = undefined;
+missioncomplete = undefined;
+flag = undefined;
+flag2 = undefined;
+turncounter = undefined;
 
-userInfo = {
+newUserStats = {
 	username: "",
-	login: "",
-	level: 0,
-	hp: 10,
+	level: levels.naif,
+	hp: 25,
 	gold: 25,
 	xp: 0,
 	items: { 
 		weapon: weapons.start.hands,
-		armor: armor.start.shirt
+		armor: armor.start.shirt,
+		magic: [],
+		other: []
 	},
-	turnsToday: 0,
-	lastPlayed: 24
+	turnsToday: 20,
+	lastPlayed: 0,
+	attributes: {
+		speed: 0,
+		strength: 0,
+		charisma: 0,
+		cunning: 0
+	},
+	mission: undefined,
+	granflag: true 
 }
 
 // user FUNCTIONS
@@ -170,11 +343,21 @@ userInfo = {
 CheckTurnsToday = function () {
 	var today = new Date();
 	var day = today.getDate();
-	if (userInfo.lastPlayed === day && userInfo.turnsToday >= 20) {
+	if (userInfo.lastPlayed === day && userInfo.turnsToday <= 0) {
 		clear();
-		output(1, "You are fatigued from too much fighting! You must keep inside town until tomorrow.<br><br>");
+		output(1, "You are fatigued! You must keep inside town until tomorrow.<br><br>");
 		thread = 1.9;
 		return true;
 	}
 	userInfo.lastPlayed = day;
+}
+
+levelup = function(x){
+	if (x===2){
+		userInfo.level = levels.apprentice;
+		userInfo.hp = levels.apprentice.maxhp;
+	} else if (x===3){
+		userInfo.level = challenger;
+		userInfo.hp = levels.challenger.maxhp;
+	}
 }
